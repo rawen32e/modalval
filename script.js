086e6476b6e3f4b12752d6010253a91e65386ce5
Тележкин Владимир Алексеевич <320143@montagg.ru>
2022-11-21 16:31:50 +0500

    add js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.querySelector(".button")
-document.querySelector(".name")
-document.querySelector(".age")
+document.querySelector("#btnRes").addEventListener("click", function(){
+    let  name = document.querySelector("#name")
+    let  age = document.querySelector("#age")
+    let str =''
+    if(age.value <= 20){
+        str =`<p>${name.value} вы молод вам ${age.value} лет</p>`
+    }
+    else if (age.value > 20){
+        str =`<p>${name.value} вы стар вам ${age.value} лет</p>`
+    }
+    document.querySelector(".rezult").insertAdjacentElement("beforeend", str)
+})
